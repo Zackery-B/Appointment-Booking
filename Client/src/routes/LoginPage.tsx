c5a61bc7e2bb25d2a2e55a6f997d1fb22a899f8d
Zack <06zackery@gmail.com>
2026-05-10 19:52:05 -0400

    Add login page

--- Client/src/routes/LoginPage.tsx
+++ Client/src/routes/LoginPage.tsx
@@ -1,10 +1,106 @@
 import { useState } from "react";
-import type { ChangeEvent, FormEvent } from "react";
+import type { ChangeEvent, SubmitEvent } from "react";
+import { useNavigate, Link } from 'react-router';
+import { useAuth } from "../contexts/AuthContext";
+//import type { User } from "../types/types";
+//import "../styles/LoginPage.css";
 
 export default function LoginPage() {
+
+    const { login } = useAuth(); // Destructure needed pieces of auth object
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState(false);
+
+    // updates form data when its changed in the form 
+    function updateFormData(ev: ChangeEvent<HTMLInputElement>) {
+        const changedName = ev.target.name;
+        const newValue = ev.target.value;
+        setFormData((prevData) => {
+            return { ...prevData, [changedName]: newValue };
+        });
+    }
+
+    // manage login attempt 
+    function handleSubmit(submission: SubmitEvent<HTMLFormElement>){
+        submission.preventDefault();
+        
+        if (formData.email == "" || formData.password == ""){
+            setError(true);
+        }
+        else {
+            
+            // make request to api to validate login 
+            fetch("http://localhost:3001/user/login",{
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+            .then((response) => { // manage api response 
+                if (!response.ok)
+                throw new Error(`***HTTP error, status: ${response.status}`); 
+
+                return response.json();
+            })
+            .then((data) => {  // manage received data 
+                
+                let userData = data.user;
+
+                login(userData); // mark user as logged in
+                
+                navigate('/appointments');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(true); 
+            });
+        }
+    }
+
     return(
-        <section>
-            <p>this is the login page</p>
-        </section>
+    <section className="LoginPage">
+        <form onSubmit={handleSubmit}>
+            <div>
+                <h2>Login to view your appointments</h2>
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                type="text"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={updateFormData}
+                />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                type="password"
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={updateFormData}
+                />
+            </div>
+            <div>
+                <button>Log In</button>
+            </div>
+            <div>
+                <p>Don't have a account ?</p>
+                <Link to="/signup">Make one</Link>
+            </div>
+            {error && <div className="error">Login failed</div>}
+        </form>
+    </section>
     )
 }
