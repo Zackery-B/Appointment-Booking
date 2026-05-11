6010789bc56993f08bd36ef28d40248d641daa77
Zack <06zackery@gmail.com>
2026-05-10 23:55:07 -0400

    Add the basic structure of TimeSlotSelector

---
+++ Client/src/components/TimeSlotSelector.tsx
@@ -0,0 +1,12 @@
+
+type TimeSlotSelectorProps = {
+    selectedSlot: Number | null;
+    setSelectedSlot: (slot:any) => void;
+};
+
+export default function TimeSlotSelector( {selectedSlot, setSelectedSlot}: TimeSlotSelectorProps) {
+    return(
+        <>
+        </>
+    )
+}
