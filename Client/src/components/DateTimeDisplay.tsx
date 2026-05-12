6963c12da3d86505401ec88b55b9779e24658cac
Zack <06zackery@gmail.com>
2026-05-12 14:12:01 -0400

    Make the time display better

--- Client/src/components/DateTimeDisplay.tsx
+++ Client/src/components/DateTimeDisplay.tsx
@@ -2,10 +2,26 @@ type DateTimeProps = {
     datetime: string;
 };
 
+const formatterDate = new Intl.DateTimeFormat("en-CA", {
+  month: "short",
+  day: "numeric",
+  year: "numeric"
+});
+
+const formatterTime = new Intl.DateTimeFormat("en-CA", {
+  hour: "numeric",
+  minute: "2-digit",
+  hour12: true
+});
+
 export default function DateTimeDisplay({datetime}:DateTimeProps) {
+
+    const date = new Date(datetime.replace(" ", "T")) // better safari support
+    
     return(
-        <p>
-            {new Date(datetime).toLocaleString()}
-        </p>
+        <div>
+            <span>{`${formatterDate.format(date)} `}</span>
+            <span>{`at ${formatterTime.format(date)}`}</span>
+        </div>
     )
 }
