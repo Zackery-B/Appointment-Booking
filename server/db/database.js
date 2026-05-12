8cbe01760e3af9588d5a7466036eab07c00f6ed8
Zack <06zackery@gmail.com>
2026-05-11 20:48:14 -0400

    Correct error in reason field of appointments table

--- server/db/database.js
+++ server/db/database.js
@@ -44,7 +44,7 @@ db.serialize(() => {
         CREATE TABLE IF NOT EXISTS appointments (
             slot_id INTEGER PRIMARY KEY AUTOINCREMENT,  
             status TEXT NOT NULL CHECK(status IN ('pending', 'confirmed', 'cancelled')),
-            reason,
+            reason TEXT,
             details TEXT, 
             time_slot_id INTEGER,
             client_id INTEGER,
