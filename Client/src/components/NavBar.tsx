8ca65e9f69e5077a9c2b08734257b89e62c2960e
Zack <06zackery@gmail.com>
2026-05-12 14:12:31 -0400

    Add appointments and appointment booking to the nav bar

--- Client/src/components/NavBar.tsx
+++ Client/src/components/NavBar.tsx
@@ -10,6 +10,8 @@ export default function NavBar() {
 
   return (
     <nav className="NavBar">
+      {(location.pathname != "/appointments" && user != null) ? <NavLink className="NavLink" to="/appointments">Appointments</NavLink> : ""}
+      {(location.pathname != "/appointments/book" && user != null) ? <NavLink className="NavLink" to="/appointments/book">Book Appointments</NavLink> : ""}
       {(location.pathname != "/login" && user == null) ? <NavLink className="NavLink" to="/login">Login</NavLink> : ""}
       {user != null && <button onClick={logout}>Logout</button>}
     </nav>
