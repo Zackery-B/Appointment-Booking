1c5dd57f4e0213b7b90eb6e2cdd5013b402bf235
Zack <06zackery@gmail.com>
2026-05-12 14:54:30 -0400

    Add a filter by doctor function to the time slot selector

--- Client/src/components/TimeSlotSelector.tsx
+++ Client/src/components/TimeSlotSelector.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import type { ChangeEvent } from "react";
 import TimeSlotDisplay from "./TimeSlotDisplay";
 import { type TimeSlot } from "../types/types";
 
@@ -10,23 +11,56 @@ type TimeSlotSelectorProps = {
 
 export default function TimeSlotSelector( {timeSlots, setSelectedSlot}: TimeSlotSelectorProps) {
 
-    if (!timeSlots || timeSlots.length === 0) return (
-        <p>No time slots available</p>
-    );
+    const [selectedDoctor, setSelectedDoctor] = useState(""); 
+
+    function handleDoctorSelection(ev: ChangeEvent<HTMLSelectElement>){
+        setSelectedDoctor(()=>{
+            return ev.target.value
+        })
+    }
 
     return(
-        <ul>
-        {timeSlots.map((timeSlot) => (
-          <li>
-            <TimeSlotDisplay 
-                timeSlot={timeSlot}
-                setSelectedSlot={
-                    (slot:TimeSlot) => {setSelectedSlot(slot)}
-                }
-                key={timeSlot.id}
-            />
-          </li>
-        ))}
-        </ul>
+        (!timeSlots || timeSlots.length === 0) 
+        ? (
+            <p>No time slots available</p>
+        ) : (
+            <>
+            <div>
+                <label htmlFor="doctor">Filter by doctor</label>
+                <select
+                name="doctor"
+                id="doctor"
+                value={selectedDoctor}
+                onChange={handleDoctorSelection}
+                >
+                    <option value="">Select a doctor</option>
+
+                    {timeSlots.map((timeSlot) => ( // add all doctors with a time slot 
+                        <option value={timeSlot.doctorLastName}>{timeSlot.doctorFirstName} {timeSlot.doctorLastName}</option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <ul>
+                {timeSlots.map((timeSlot) => (
+                    ( selectedDoctor == "" || selectedDoctor == timeSlot.doctorLastName) // filter by last name 
+                    ? (
+                        <li>
+                        <TimeSlotDisplay 
+                            timeSlot={timeSlot}
+                            setSelectedSlot={
+                                (slot:TimeSlot) => {setSelectedSlot(slot)}
+                            }
+                            key={timeSlot.id}
+                        />
+                        </li>
+                    ) : ( 
+                        <></> // nothing
+                    )
+                ))}
+                </ul>
+            </div>
+            </>
+        )
     )
 }
