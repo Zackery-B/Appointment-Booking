bf128b94fcaa7b4b6d91bfa2f64aee20205447ed
Zack <06zackery@gmail.com>
2026-05-11 20:11:38 -0400

    Add updatable form to book appointments

--- Client/src/routes/BookAppointmentsPage.tsx
+++ Client/src/routes/BookAppointmentsPage.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router";
-import { useState, useEffect, type SubmitEvent} from "react";
+import { useState, useEffect } from "react";
+import type { SubmitEvent, ChangeEvent} from "react";
 import TimeSlotSelector from "../components/TimeSlotSelector";
 import Drawer from "../components/Drawer";
 import { type TimeSlot } from "../types/types";
@@ -12,8 +13,13 @@ export default function BookAppointmentPage() {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const [timeSlots, setTimeSlots] = useState<TimeSlot[]>([]);
     const [selectedSlot, setSelectedSlot] = useState<TimeSlot|null>(null);
+    
+    const [formData, setFormData] = useState({
+        reason: "",
+        details: "",
+    });
 
-    // make sure user is logged then in on page load
+    // on page load, make sure user is logged in then get time slots
     useEffect(() => { 
         //if (user == null)  // removed for testing --------------------- testing 
             //navigate('/login');
@@ -33,6 +39,15 @@ export default function BookAppointmentPage() {
         });
     }
 
+    // updates form data when its changed in the form 
+    function updateFormData(ev: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) {
+        const changedName = ev.target.name;
+        const newValue = ev.target.value;
+        setFormData((prevData) => {
+            return { ...prevData, [changedName]: newValue };
+        });
+    }
+
     // handle form submission 
     function handleSubmit(submission: SubmitEvent<HTMLFormElement>){
         submission.preventDefault();
@@ -43,16 +58,39 @@ export default function BookAppointmentPage() {
     return(
     <section className = "BookAppointmentPage">
         <form onSubmit={handleSubmit}>
-            <button type="button" onClick={() => setDrawerOpen(true)} > 
-                {(selectedSlot == null) ? (
-                    <p>Select time slot</p>
-                ) : (
-                    <>
-                    <p>Dr. {selectedSlot.doctorFirstName} {selectedSlot.doctorLastName}</p>
-                    <p>Time: {selectedSlot.datetime}</p>
-                    </>
-                )}
-            </button>
+            <div>
+                <label htmlFor="reason">Reason</label>
+                <select
+                name="reason"
+                id="reason"
+                value={formData.reason}
+                onChange={updateFormData}
+                >
+                    <option value="">Select a reason for the appointment</option>
+                    <option value="client">Client</option>
+                    <option value="doctor">Doctor</option>
+                </select>
+            </div>
+            <div>
+                <textarea 
+                name="details" 
+                id="details"
+                value={formData.details}
+                onChange={updateFormData}
+                ></textarea>
+            </div>
+            <div>
+                <button type="button" onClick={() => setDrawerOpen(true)} > 
+                    {(selectedSlot == null) ? (
+                        <p>Select time slot</p>
+                    ) : (
+                        <>
+                        <p>Dr. {selectedSlot.doctorFirstName} {selectedSlot.doctorLastName}</p>
+                        <p>Time: {selectedSlot.datetime}</p>
+                        </>
+                    )}
+                </button>
+            </div>
             <button>Complete Appointment</button>
         </form>
 
