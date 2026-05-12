4f7a91b6baa7a715bc27a5004669b34849730304
Zack <06zackery@gmail.com>
2026-05-12 14:59:38 -0400

    Add automatic redirect to login page when logging out

--- Client/src/components/NavBar.tsx
+++ Client/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
 import { useLocation } from 'react-router';
 import "../styles/NavBar.css";
@@ -6,14 +6,20 @@ import "../styles/NavBar.css";
 export default function NavBar() {
   const { user, logout} = useAuth(); // destructure auth object
 
+  const navigate = useNavigate();
   const location = useLocation();
+
+  function handleLogout() {
+    logout();
+    navigate("/login");
+  };
 
   return (
     <nav className="NavBar">
       {(location.pathname != "/appointments" && user != null) ? <NavLink className="NavLink" to="/appointments">Appointments</NavLink> : ""}
       {(location.pathname != "/appointments/book" && user != null) ? <NavLink className="NavLink" to="/appointments/book">Book Appointments</NavLink> : ""}
       {(location.pathname != "/login" && user == null) ? <NavLink className="NavLink" to="/login">Login</NavLink> : ""}
-      {user != null && <button onClick={logout}>Logout</button>}
+      {user != null && <button onClick={handleLogout}>Logout</button>}
     </nav>
   )
 }
