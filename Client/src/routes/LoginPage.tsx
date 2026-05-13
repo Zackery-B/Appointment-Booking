f448887910b2b0daa8a80a290c82c4c1003f51ab
Zack <06zackery@gmail.com>
2026-05-12 21:41:48 -0400

    Remove unneeded line

--- Client/src/routes/LoginPage.tsx
+++ Client/src/routes/LoginPage.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import type { ChangeEvent, SubmitEvent } from "react";
 import { useNavigate, Link } from 'react-router';
 import { useAuth } from "../contexts/AuthContext";
-//import type { User } from "../types/types";
 //import "../styles/LoginPage.css";
 
 export default function LoginPage() {
