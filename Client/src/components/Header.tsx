015c7dce321b8a5691165516015af601b26afc81
Zack <06zackery@gmail.com>
2026-05-13 00:39:47 -0400

    Correct head and nav bar styles and Header content

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 export default function Header() {
     return(
     <header className="Header">
-        <h1>Patient Bookings</h1>
+        <h1>Appointment Bookings</h1>
         <NavBar/>
     </header>
     )
